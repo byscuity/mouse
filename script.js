b4e200a80c41bd1c3141b39f889c983e3d660805
byscuity <joao.alves.farias@escola.pr.gov.br>
2026-07-27 08:54:43 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let banner = document.querySelector('.banner');
+banner.onmousemove = fuction(e){
+ let
+ let
+ banner.style.setProperty('--x', x + 'px');
+ banner.style.setProperty('--y', y = 'px');
