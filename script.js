67557f700bdbc240509035c888ff62c92813aeb1
byscuity <joao.alves.farias@escola.pr.gov.br>
2026-07-27 11:47:27 -0300

    Correct mousemove function and variable usage
    Fix typo in function name and correct variable assignments.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let banner = document.querySelector('.banner');
 banner.onmousemove = fuction(e){
- let
- let
+ let x = e.pagex - banner.offsetLeft;
+ let y = e.pagey - banner.offsetTop;
  banner.style.setProperty('--x', x + 'px');
- banner.style.setProperty('--y', y = 'px');
+ banner.style.setProperty('--y', y + 'px');
